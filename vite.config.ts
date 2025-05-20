2e360492fdb2f53d51e30ad5484c03caafb4b058
TWAYINGANYIKI PROMIS LEONCE <93355028+leopromus@users.noreply.github.com>
2025-05-20 14:10:15 +0100

    Add fronte_nd fix

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: '/',
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      },
+      '/ws': {
+        target: 'ws://localhost:8080',
+        ws: true
+      }
+    }
+  },
+  define: {
+    global: 'window',
+  }
+}))
